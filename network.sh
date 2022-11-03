86868d07f7a46021ebcbd3669a8fae26aeca08fa
Pranav R R <pranav.rk02@gmail.com>
2022-11-03 11:15:58 +0530

    Add functions for creating Organization Identities

--- network.sh
+++ network.sh
@@ -14,6 +14,22 @@ export VERBOSE=false
 : ${CONTAINER_CLI_COMPOSE:="${CONTAINER_CLI}-compose"}
 infoln "Using ${CONTAINER_CLI} and ${CONTAINER_CLI_COMPOSE}"
 
+# Obtain CONTAINER_IDS and remove them
+# This function is called when you bring a network down
+function clearContainers() {
+    infoln "Removing remaining containers"
+    ${CONTAINER_CLI} rm -f $(${CONTAINER_CLI} ps -aq --filter label=service=hyperledger-fabric) 2>/dev/null || true
+    ${CONTAINER_CLI} rm -f $(${CONTAINER_CLI} ps -aq --filter name='dev-peer*') 2>/dev/null || true
+}
+
+# Delete any images that were generated as a part of this setup
+# specifically the following images are often left behind:
+# This function is called when you bring the network down
+function removeUnwantedImages() {
+    infoln "Removing generated chaincode docker images"
+    ${CONTAINER_CLI} image rm -f $(${CONTAINER_CLI} images -aq --filter reference='dev-peer*') 2>/dev/null || true
+}
+
 # Versions of fabric known not to work with the medtrack application
 NONWORKING_VERSIONS="^1\.0\. ^1\.1\. ^1\.2\. ^1\.3\. ^1\.4\."
 
@@ -71,4 +87,80 @@ function checkPrereqs() {
     if [ "$CA_LOCAL_VERSION" != "$CA_DOCKER_IMAGE_VERSION" ]; then
         warnln "Local fabric-ca binaries and docker images are out of sync. This may cause problems."
     fi
+}
+
+# Before you can bring up a network, each organization needs to generate the crypto
+# material that will define that organization on the network. Because Hyperledger
+# Fabric is a permissioned blockchain, each node and user on the network needs to
+# use certificates and keys to sign and verify its actions. In addition, each user
+# needs to belong to an organization that is recognized as a member of the network.
+# You can use the Cryptogen tool or Fabric CAs to generate the organization crypto
+# material.
+
+# By default, the sample network uses cryptogen. Cryptogen is a tool that is
+# meant for development and testing that can quickly create the certificates and keys
+# that can be consumed by a Fabric network. The cryptogen tool consumes a series
+# of configuration files for each organization in the "organizations/cryptogen"
+# directory. Cryptogen uses the files to generate the crypto  material for each
+# org in the "organizations" directory.
+
+# You can also use Fabric CAs to generate the crypto material. CAs sign the certificates
+# and keys that they generate to create a valid root of trust for each organization.
+# The script uses Docker Compose to bring up three CAs, one for each peer organization
+# and the ordering organization. The configuration file for creating the Fabric CA
+# servers are in the "organizations/fabric-ca" directory. Within the same directory,
+# the "registerEnroll.sh" script uses the Fabric CA client to create the identities,
+# certificates, and MSP folders that are needed to create the test network in the
+# "organizations/ordererOrganizations" directory.
+
+# Create Organization crypto material using cryptogen or CAs
+function createOrgs() {
+    if [ -d "organizations/peerOrganizations" ]; then
+        rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
+    fi
+
+    # Create crypto material using Fabric CA
+    infoln "Generating certificates using Fabric CA"
+    ${CONTAINER_CLI_COMPOSE} -f compose/$COMPOSE_FILE_CA -f compose/$CONTAINER_CLI/${CONTAINER_CLI}-$COMPOSE_FILE_CA up -d 2>&1
+
+    . organizations/fabric-ca/registerEnroll.sh
+
+    while :; do
+        if [ ! -f "organizations/fabric-ca/org1/tls-cert.pem" ]; then
+            sleep 1
+        else
+            break
+        fi
+    done
+
+    infoln "Creating Supplier Organization Identities"
+
+    createSupplierOrg
+
+    infoln "Creating Manufacturer Organization Identities"
+
+    createManufacturerOrg
+
+    infoln "Creating Distributor Organization Identities"
+
+    createDistributorOrg
+
+    infoln "Creating Wholesaler Organization Identities"
+
+    createWholesalerOrg
+
+    infoln "Creating Retailer Organization Identities"
+
+    createRetailerOrg
+
+    infoln "Creating Consumer Organization Identities"
+
+    createConsumerOrg
+
+    infoln "Creating Orderer Org Identities"
+
+    createOrderer
+
+    infoln "Generating CCP files for all organizations"
+    ./organizations/ccp-generate.sh
 }

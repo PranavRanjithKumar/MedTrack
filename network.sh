6334b0feaa93a79491ee4b9736fab6146709265a
Pranav R R <pranav.rk02@gmail.com>
2022-11-07 12:54:00 +0530

    Add functionality to parse the arguments

--- network.sh
+++ network.sh
@@ -288,3 +288,86 @@ function networkDown() {
         ${CONTAINER_CLI} run --rm -v "$(pwd):/data" busybox sh -c 'cd /data && rm -rf channel-artifacts log.txt *.tar.gz'
     fi
 }
+
+# Using crpto vs CA. default is cryptogen
+CRYPTO="Certificate Authorities"
+# timeout duration - the duration the CLI should wait for a response from
+# another container before giving up
+MAX_RETRY=5
+# default for delay between commands
+CLI_DELAY=3
+# channel name defaults to "mychannel"
+CHANNEL_NAME="plnchannel"
+# chaincode name defaults to "NA"
+CC_NAME="NA"
+# chaincode path defaults to "NA"
+CC_SRC_PATH="NA"
+# endorsement policy defaults to "NA". This would allow chaincodes to use the majority default policy.
+CC_END_POLICY="NA"
+# collection configuration defaults to "NA"
+CC_COLL_CONFIG="NA"
+# chaincode init function defaults to "NA"
+CC_INIT_FCN="NA"
+# use this as the default docker-compose yaml definition
+COMPOSE_FILE_BASE=compose-test-net.yaml
+# docker-compose.yaml file if you are using couchdb
+COMPOSE_FILE_COUCH=compose-couch.yaml
+# certificate authorities compose file
+COMPOSE_FILE_CA=compose-ca.yaml
+#
+# chaincode language defaults to "NA"
+CC_SRC_LANGUAGE="NA"
+# default to running the docker commands for the CCAAS
+CCAAS_DOCKER_RUN=true
+# Chaincode version
+CC_VERSION="1.0"
+# Chaincode definition sequence
+CC_SEQUENCE=1
+# default database
+DATABASE="couchdb"
+
+# Get docker sock path from environment variable
+SOCK="${DOCKER_HOST:-/var/run/docker.sock}"
+DOCKER_SOCK="${SOCK##unix://}"
+
+# Parse commandline args
+## Parse mode
+if [[ $# -lt 1 ]]; then
+    printHelp
+    exit 0
+else
+    MODE=$1
+    shift
+fi
+
+#Unknown arguments if passed
+if [[ $# -ge 1 ]]; then
+    key="$1"
+    errorln "Unknown flag: $key"
+    printHelp
+    exit 1
+fi
+
+# Are we generating crypto material with this command?
+if [ ! -d "organizations/peerOrganizations" ]; then
+    CRYPTO_MODE="with crypto from '${CRYPTO}'"
+else
+    CRYPTO_MODE=""
+fi
+
+# Determine mode of operation and printing out what we asked for
+if [ "$MODE" == "up" ]; then
+    infoln "Starting nodes with CLI timeout of '${MAX_RETRY}' tries and CLI delay of '${CLI_DELAY}' seconds and using database '${DATABASE} ${CRYPTO_MODE}"
+    infoln "Creating channel '${CHANNEL_NAME}'."
+    createChannel
+elif [ "$MODE" == "down" ]; then
+    infoln "Stopping network"
+    networkDown
+elif [ "$MODE" == "restart" ]; then
+    infoln "Restarting network"
+    networkDown
+    networkUp
+else
+    printHelp
+    exit 1
+fi

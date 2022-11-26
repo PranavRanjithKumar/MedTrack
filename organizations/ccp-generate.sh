f36699f4f11e9f74b0094abe73f1acccfd1f2797
Pranav R R <pranav.rk02@gmail.com>
2022-11-26 10:59:37 +0530

    Fix bug

--- organizations/ccp-generate.sh
+++ organizations/ccp-generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function one_line_pem {
-    echo "$(awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1)"
+    echo "`awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1`"
 }
 
 function json_ccp {

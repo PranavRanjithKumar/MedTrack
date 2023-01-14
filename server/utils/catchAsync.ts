393b461f9615adfb7fd48502ae2501f8e680db4f
Pranav R R <pranav.rk02@gmail.com>
2023-01-15 00:16:46 +0530

    Change promise return type

--- server/utils/catchAsync.ts
+++ server/utils/catchAsync.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
+import AppError from './AppError';
 
 type CatchAyncFn = (
-  arg0: (req: Request, res: Response, next: NextFunction) => Promise<void>
+  arg0: (req: Request, res: Response, next: NextFunction) => Promise<any>
 ) => RequestHandler;
 
 const catchAsync: CatchAyncFn = (fn) => (req, res, next) => {

45b9d4db5e1395dd63543e0067c293d3be3cdff9
Pranav R R <pranav.rk02@gmail.com>
2023-01-22 12:56:33 +0530

    Add Register User Wallet fn

--- server/utils/CAUtils.ts
+++ server/utils/CAUtils.ts
@@ -1,5 +1,7 @@
-import FabricCAServices from 'fabric-ca-client';
+import FabricCAServices, { IKeyValueAttribute } from 'fabric-ca-client';
 import { Wallet } from 'fabric-network';
+import { IUser } from '../models/userModel';
+import AppError from './AppError';
 
 const buildCAClient = (
   ccp: Record<string, any>,
@@ -55,4 +57,59 @@ const enrollAdmin = async (
   }
 };
 
-export { buildCAClient, enrollAdmin };
+const registerAndEnrollUser = async (
+  caClient: FabricCAServices,
+  wallet: Wallet,
+  orgMspId: string,
+  role: string,
+  adminUserId: string,
+  userAttributes: IKeyValueAttribute[],
+  newUser: IUser,
+  affiliation: string
+): Promise<void> => {
+  // Check to see if we've already enrolled the user
+  const userIdentity = await wallet.get(newUser.email);
+  if (userIdentity) {
+    throw new AppError('An identity with this userId already exists!', 400);
+  }
+
+  // Must use an admin to register a new user
+  const adminIdentity = await wallet.get(adminUserId);
+  if (!adminIdentity) {
+    throw new AppError(
+      'An identity for this admin user does not exist in the wallet',
+      400
+    );
+  }
+
+  // build a user object for authenticating with the CA
+  const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+  const adminUser = await provider.getUserContext(adminIdentity, adminUserId);
+
+  // Register the user, enroll the user, and import the new identity into the wallet.
+  // if affiliation is specified by client, the affiliation value must be configured in CA
+  const secret = await caClient.register(
+    {
+      affiliation,
+      enrollmentID: newUser.email,
+      attrs: userAttributes,
+      role,
+    },
+    adminUser
+  );
+  const enrollment = await caClient.enroll({
+    enrollmentID: newUser.email,
+    enrollmentSecret: secret,
+  });
+  const x509Identity = {
+    credentials: {
+      certificate: enrollment.certificate,
+      privateKey: enrollment.key.toBytes(),
+    },
+    mspId: orgMspId,
+    type: 'X.509',
+  };
+  await wallet.put(newUser.email, x509Identity);
+};
+
+export { buildCAClient, enrollAdmin, registerAndEnrollUser };
